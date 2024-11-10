3ea8eba4a37781b07d1160acc9fb43adcc9ae2a9
Michael Albrecht <albre@MLaptop.(none)>
2024-11-10 15:31:47 +0100

    Files für den Server

--- Chat App/server.js
+++ Chat App/server.js
@@ -1,30 +1,11 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const PORT = process.env.PORT || 3000;
 
-// Statische Dateien im Ordner "public" bereitstellen
-app.use(express.static('public'));
-
-// Event wenn sich ein Client verbindet
-io.on('connection', (socket) => {
-    console.log('Ein Benutzer hat sich verbunden.');
-
-    // Empfang und Weiterleitung von Nachrichten an alle Clients
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-
-    // Benutzer hat die Verbindung getrennt
-    socket.on('disconnect', () => {
-        console.log('Benutzer hat die Verbindung getrennt.');
-    });
+app.get('/', (req, res) => {
+  res.send('Hello, Docker with Node.js du stinkst!');
 });
 
-// Server starten
-server.listen(3000, () => {
-    console.log('Server läuft auf http://localhost:3000');
-});
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
